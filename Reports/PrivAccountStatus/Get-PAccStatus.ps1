76087f39f2aec601f02792a5d00883926527a1c4
Rafael Rocha <91136995+rdfrocha@users.noreply.github.com>
2025-12-26 13:26:52 -0500

    Add license and disclaimer sections to Get-PAccStatus.ps1

--- Reports/PrivAccountStatus/Get-PAccStatus.ps1
+++ Reports/PrivAccountStatus/Get-PAccStatus.ps1
@@ -52,6 +52,36 @@
     2. Export to the credentials directory:
        $creds | Export-Clixml -Path "C:\Credentials\$env:USERNAME@$env:computername-Access.xml"
     3. The file will be encrypted using DPAPI and can only be decrypted by the same user on the same computer.
+
+.LICENSE
+
+    MIT License
+
+    Copyright (c) 2025 Rafael da Rocha
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+.DISCLAIMER
+    This script is provided "as-is" without any warranty or guarantee of its functionality or suitability for any purpose. 
+    Use at your own risk. The author is not responsible for any issues, damages, or losses that may arise from using this script.
+    Ensure you test the script in a controlled environment before deploying it in production.
+
 #>
 
 param (
